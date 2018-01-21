28f3294a04b85253fd872973d9c11635d5948f63
Difegue <sugoi@cock.li>
2018-01-21 19:59:52 +0100

    Install script complete
    Signed-off-by: Difegue <sugoi@cock.li>

--- tools/install.pl
+++ tools/install.pl
@@ -3,9 +3,32 @@
 use strict;
 use warnings;
 use utf8;
+use Cwd;
 
 use local::lib;
 use feature qw(say);
+
+#Vendor dependencies
+my @vendor_css =("/blueimp-file-upload/css/jquery.fileupload.css",
+                 "/datatables/media/css/jquery.dataTables.min.css",
+                 "/fontawesome-web/css/fontawesome-all.min.css",
+                 "/jb-dropit/dropit.css",
+                 "/jqcloud2/dist/jqcloud.min.css",
+                 "/jquery-toast-plugin/dist/jquery.toast.min.css");
+
+my @vendor_js = ("/blueimp-file-upload/js/jquery.fileupload.js",
+                 "/datatables/media/js/jquery.dataTables.min.js",
+                 "/jb-dropit/dropit.js",
+                 "/jqcloud2/dist/jqcloud.min.js",
+                 "/jquery/dist/jquery.min.js",
+                 "/jquery-toast-plugin/dist/jquery.toast.min.js");
+
+my @vendor_woff=("/fontawesome-web/webfonts/fa-solid-900.woff",
+                 "/open-sans-fontface/fonts/Regular/OpenSans-Regular.woff",
+                 "/open-sans-fontface/fonts/Bold/OpenSans-Bold.woff",
+                 "/roboto-fontface/fonts/roboto/Roboto-Regular.woff",
+                 "/roboto-fontface/fonts/roboto/Roboto-Bold.woff");
+
 
 say("             . .. .     MMMMMMMM........");
 say("             . .. . MMMMMM. ....MM .....");
@@ -35,26 +58,34 @@ say("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
 say("~~~~~~~~~~~LANraragi Installer~~~~~~~~~~");
 say("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
 
-#Load IPC::Cmd
-eval "require IPC::Cmd";
-if( $@ ) {
-   say ("IPC::Cmd not installed! Trying to install now using cpanm.");
-   system("cpanm IPC::Cmd");
-   require IPC::Cmd;
-}
-else {
-  say ("IPC::Cmd package installed, proceeding...");
+unless (@ARGV > 0 ) {
+  say ("Execution: npm run lanraragi-installer [mode]");
+  say ("--------------------------");
+  say ("Available modes are: ");
+  say ("* install-front: Install/Update Clientside dependencies.");
+  say ("* install-back: Install/Update Perl dependencies.");
+  say ("* install-full: Install/Update all dependencies.");
+  say ("");
+  say ("If using a prepackaged release, you probably only need install-back.");
+  exit;
 }
 
+my $front = $ARGV[0] eq "install-front";
+my $back = $ARGV[0] eq "install-back";
+my $full = $ARGV[0] eq "install-full";
+
+say("Working Directory: ".getcwd);
+say("");
+
+#Load IPC::Cmd
+install_package("IPC::Cmd");
 IPC::Cmd->import( 'can_run' );
 
-say ("");
-say ("Will now check if all LRR software dependencies are met.");
-say ("");
+say ("\r\n Will now check if all LRR software dependencies are met. \r\n");
 
 #Check for Redis
 print ("Checking for Redis...");
-can_run('redis-cli') or die 'NOT FOUND! \r\n Please install a Redis server before proceeding.';
+can_run('redis-server') or die 'NOT FOUND! \r\n Please install a Redis server before proceeding.';
 say ("OK!");
 
 #Check for unar
@@ -78,20 +109,71 @@ else {
 }
 
 #Build & Install CPAN Dependencies 
-say ("");
-say ("Installing Perl modules... This might take a while.");
-say ("");
+if ($back || $full) {
+  say ("\r\nInstalling Perl modules... This might take a while.\r\n");
 
-system("cpanm --installdeps ./tools/.");
+  system("cpanm --installdeps ./tools/.");
+}
 
-#Webpack Clientside Dependencies
-say ("");
-say ("Installing Web dependencies...");
-say ("");
+#Clientside Dependencies with Provisioning
+if ($front || $full) {
 
-system("npm install");
+  say ("\r\nObtaining remote Web dependencies...\r\n");
+
+  system("npm install");
+
+  say ("\r\nProvisioning...\r\n");
+  #Load File::Copy
+  install_package("File::Copy");
+  File::Copy->import("copy");
+
+  mkdir getcwd."/public/css/vendor";
+  mkdir getcwd."/public/js/vendor";
+
+  for my $css (@vendor_css) {
+    cp_node_module($css, "/public/css/vendor/");
+  }
+
+  for my $js (@vendor_js) {
+    cp_node_module($js, "/public/js/vendor/");
+  }
+
+  for my $woff (@vendor_woff) {
+    cp_node_module($woff, "/public/css/vendor/");
+  }
+  
+}
 
 #Done!
-say ("");
-say ("All set! You can start LANraragi by typing the command: ");
+say ("\r\nAll set! You can start LANraragi by typing the command: ");
 say ("npm start");
+
+
+sub cp_node_module {
+
+  my ($item, $newpath) = @_;
+
+  my $nodename = getcwd."/node_modules".$item;
+  $item =~ /([^\/]+$)/;
+  my $newname = getcwd.$newpath.$&;
+
+  say ("Copying $nodename \r\n to $newname \r\n");
+  copy($nodename , $newname) or die "The copy operation failed: $!";
+
+}
+
+sub install_package {
+
+  my $package = $_[0];
+
+  eval "require $package";
+  if( $@ ) {
+     say ("$package not installed! Trying to install now using cpanm.");
+     system("cpanm $package");
+     require $package;
+  }
+  else {
+    say ("$package package installed, proceeding...");
+  }
+
+}
