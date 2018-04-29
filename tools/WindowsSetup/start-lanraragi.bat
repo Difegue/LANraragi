50e0edcbc4052f37503c840ce1c57fc28eaefdae
difegue <sugoi@cock.li>
2018-04-29 21:56:53 +0200

    Finished up the Windows QuickStarter (#9)

--- tools/WindowsSetup/start-lanraragi.bat
+++ tools/WindowsSetup/start-lanraragi.bat
@@ -1,33 +1,40 @@
 @echo off
-rem LRR Windows Boot Script - super crude edition
+rem LRR Windows Boot Script
 
-echo LANraragi Easy-Windows Startup
+echo LANraragi Windows QuickStarter
 echo ==============
 echo 
 
 rem Check through berrybrew if the correct perl version is installed
+:check
 echo Checking for Perl installation...
 
-IF EXIST C:\berrybrew\5.26.0_64 goto :exec ELSE goto :brewing
+IF EXIST C:\berrybrew\5.26.0_64 (
+	goto :exec 
+) ELSE (
+	goto :brewing
+) 
 
+rem add perl/unar/lsar to path and start redis-server + LRR with daemon
 :exec
-rem add unar/lsar to path and start redis-server + LRR with daemon
-echo Perl installation good to go! Starting up...
 SET PATH=%PATH%;%~dp0unar
 set PATH=C:\berrybrew\5.26.0_64\perl\site\bin;C:\berrybrew\5.26.0_64\perl\bin;C:\berrybrew\5.26.0_64\c\bin;%PATH%
+echo Perl OK. Checking and installing LRR dependencies.
+call cpanm --installdeps ./lanraragi/tools/. --force 
 
-.\redis\redis-server.exe &
-cd .\lanraragi &
+echo All dependencies up to date! Starting...
+start cmd /c .\redis\redis-server.exe 
+cd .\lanraragi 
 perl .\script\lanraragi daemon 
 exit 0
 
-rem If install perl alongside the magick ppm and the cpan deps
+
+rem --------------------------
+rem Install perl alongside the magick ppm and the cpan deps
 :brewing
 echo No compatible Perl installation detected - Asking berrybrew to brew one up.
 .\berrybrew\bin\berrybrew.exe install 5.26.0_64
 echo Installing PerlMagick...You'll have to press Enter at least once.
 .\berrybrew\bin\berrybrew.exe exec --with 5.26.0_64 ppm install Image-Magick
-echo Installing LRR dependencies.
-.\berrybrew\bin\berrybrew.exe exec --with 5.26.0_64 cpanm --installdeps ./lanraragi/tools/. --force
 echo All done! Moving up.
-goto :exec
+goto :check
