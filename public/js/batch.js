180e3fb4ccc9f989b028285eb3da4deca2e3f0db
difegue <sugoi@cock.li>
2018-12-26 17:14:15 +0100

    Front-end cleanup

--- public/js/batch.js
+++ public/js/batch.js
@@ -1,7 +1,3 @@
-function checkAll(btn) {
-    $("input:checkbox").prop("checked", btn.checked);
-    btn.checked = !btn.checked;
-}
 
 function updatePluginArg() {
 
@@ -12,6 +8,10 @@ function updatePluginArg() {
 function startBatch() {
 
     $('.tag-options').hide();
+
+    $("#log-container").html('Started Batch Tagging operation...\n************\n');
+    $('#cancel-job').show();
+    $('#restart-job').hide();
     $('.job-status').show();
     var checkeds = document.querySelectorAll('input[name=archive]:checked');
 
@@ -27,44 +27,41 @@ function startBatch() {
         archives: arr
     };
 
-    var batchSocket = new WebSocket("ws://"+window.location.host+"/batch/socket");
+    var batchSocket = new WebSocket("ws://" + window.location.host + "/batch/socket");
 
     batchSocket.onopen = function (event) {
-        batchSocket.send(JSON.stringify(command)); 
-      };
+        batchSocket.send(JSON.stringify(command));
+    };
 
     batchSocket.onmessage = updateBatchStatus;
     batchSocket.onerror = batchError;
     batchSocket.onclose = endBatch;
 
+    $('#cancel-job').onclick = function (batchSocket) { batchSocket.close() }
 }
 
 //On websocket message, update the UI to show the archive currently being treated
 function updateBatchStatus(event) {
-    console.log(msg);
     var msg = JSON.parse(event.data);
 
-    $('#job-status').html('Processed '+msg.id + '(Added tags: '+ msg.tags +')');
-
     if (msg.success === 0) {
-        $.toast({
-            showHideTransition: 'slide',
-            position: 'top-left', 
-            loader: false, 
-            hideAfter: false,
-            heading: 'Plugin error while processing ID '+msg.id,
-            text: msg.message,
-            icon: 'warning'
-        });
+        $("#log-container").append('Plugin error while processing ID ' + msg.id + '(' + msg.message + ')\n');
+    } else {
+        $("#log-container").append('Processed ' + msg.id + '(Added tags: ' + msg.tags + ')\n');
     }
 
+    scrollLogs();
 }
 
 function batchError(event) {
+
+    $("#log-container").append('************\nError! Terminating session.\n');
+    scrollLogs();
+
     $.toast({
         showHideTransition: 'slide',
-        position: 'top-left', 
-        loader: false, 
+        position: 'top-left',
+        loader: false,
         hideAfter: false,
         heading: 'An error occured during batch tagging!',
         text: 'Please check application logs.',
@@ -77,19 +74,35 @@ function endBatch(event) {
     var status = 'info';
 
     if (event.code === 1001)
-        status = 'warning'; 
+        status = 'warning';
+
+    $("#log-container").append('************\n' + event.reason + '(code ' + event.code + ')\n');
+    scrollLogs();
 
     $.toast({
         showHideTransition: 'slide',
-        position: 'top-left', 
-        loader: false, 
-        hideAfter: false,
-        heading: event.reason,
-        text: 'Batch Tagging completed. (code ' + event.code + ')',
+        position: 'top-left',
+        loader: false,
+        heading: "Batch Tagging complete!",
+        text: '',
         icon: status
     });
 
+    $('#cancel-job').hide();
+    $('#restart-job').show();
+
+}
+
+function checkAll(btn) {
+    $(".checklist > * > input:checkbox").prop("checked", btn.checked);
+    btn.checked = !btn.checked;
+}
+
+function scrollLogs() {
+    $("#log-container").scrollTop($("#log-container").prop("scrollHeight"));
+}
+
+function restartBatchUI() {
     $('.tag-options').show();
     $('.job-status').hide();
 }
-
