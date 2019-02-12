40190fc56220bcad5d3f4c169180bb2ad4b659d5
difegue <sugoi@cock.li>
2019-02-12 23:29:05 +0100

    Daemonize redis in the testsuite action

--- .github/action-run-tests/entrypoint.sh
+++ .github/action-run-tests/entrypoint.sh
@@ -2,6 +2,9 @@
 
 echo "🎌 Running LRR Test Suite 🎌"
 
-# Start a redis server instance and run the perl tests on the repo
-/usr/bin/redis-server /home/koyomi/redis.conf & perl ./script/lanraragi test tests/*.t
+# Start a redis server instance
+/usr/bin/redis-server --daemonize yes
+
+# Run the perl tests on the repo
+perl ./script/lanraragi test tests/*.t
 
