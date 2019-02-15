3298eac6bf32158f0a69bc4859e62e996c3cf18a
difegue <sugoi@cock.li>
2019-02-15 19:09:21 +0100

    Fix formatting in critic comments

--- .github/action-critic/entrypoint.sh
+++ .github/action-critic/entrypoint.sh
@@ -8,11 +8,8 @@ echo "$violations"
 if [ $success -ne 0 ]; then
     #Report the critic violations in a comment on the commit
 
-    COMMENT="#### Perl Critic Notes:
-    <details>
-        <summary><code>'$violations'</code></summary>
-    </details>
-    "
+    COMMENT="#### Perl Critic Notes (Level 5 - gentle):
+    <pre>$violations</pre>"
     PAYLOAD=$(echo '{}' | jq --arg body "$COMMENT" '.body = $body')
     COMMIT_URL="https://api.github.com/repos/"$GITHUB_REPOSITORY/commits/$GITHUB_SHA/comments
     echo "Pushing payload to $COMMIT_URL"
