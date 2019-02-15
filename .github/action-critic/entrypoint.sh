4836b7142c3fafe553e2eb1d2e573dc2b6b3cd71
difegue <sugoi@cock.li>
2019-02-15 19:11:21 +0100

    woops the tab

--- .github/action-critic/entrypoint.sh
+++ .github/action-critic/entrypoint.sh
@@ -9,7 +9,7 @@ if [ $success -ne 0 ]; then
     #Report the critic violations in a comment on the commit
 
     COMMENT="#### Perl Critic Notes (Level 5 - gentle):
-    <pre>$violations</pre>"
+<pre>$violations</pre>"
     PAYLOAD=$(echo '{}' | jq --arg body "$COMMENT" '.body = $body')
     COMMIT_URL="https://api.github.com/repos/"$GITHUB_REPOSITORY/commits/$GITHUB_SHA/comments
     echo "Pushing payload to $COMMIT_URL"
