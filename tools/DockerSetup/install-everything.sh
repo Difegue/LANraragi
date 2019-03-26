113b42637ee2f1b943de2e340841aadee9ecf434
difegue <sugoi@cock.li>
2019-03-27 00:42:53 +0100

    fuck you alpine

--- tools/DockerSetup/install-everything.sh
+++ tools/DockerSetup/install-everything.sh
@@ -2,11 +2,7 @@
 
 #Just do everything 
 apk update 
-apk add perl perl-io-socket-ssl perl-dev g++ make pkgconf gnupg wget curl nodejs nodejs-npm redis libarchive-dev libbz2 libjpeg-turbo-dev libpng-dev openssl-dev zlib-dev supervisor git cmake musl-dev gcc gettext-dev libintl
-
-#Build musl-locales to generate the en_US.UTF8 locale (Is this even necessary?)
-git clone https://gitlab.com/rilian-la-te/musl-locales.git && cd musl-locales && git reset --hard 682c6353a7d24063be78787d45e63bfcaca78582 
-cmake . && make && make install && cd .. 
+apk add perl perl-io-socket-ssl perl-dev g++ make pkgconf gnupg wget curl nodejs nodejs-npm redis libarchive-dev libbz2 libjpeg-turbo-dev libpng-dev openssl-dev zlib-dev supervisor
 
 #Hey it's cpanm
 curl -L https://cpanmin.us | perl - App::cpanminus 
@@ -20,10 +16,10 @@ cd tools && cpanm --notest --installdeps . -M https://cpan.metacpan.org && cd ..
 npm run lanraragi-installer install-front 
 
 #Cleanup to lighten the image
-apk del perl-dev g++ make gnupg wget curl nodejs nodejs-npm openssl-dev git cmake ca-certificates musl-dev gcc gettext-dev libintl 
-rm -rf /root/.cpanm/* /usr/local/share/man/* node_modules musl-locales
+apk del perl-dev g++ make gnupg wget curl nodejs nodejs-npm openssl-dev ca-certificates
+rm -rf /root/.cpanm/* /usr/local/share/man/* node_modules 
 
 #Add the koyomi user, using the specified uid. 
 #This solves permission problems on the content folder if the Docker user sets the same uid as the owner of the folder.
 adduser -D -u $LRR_UID -g '' koyomi 
-chown -R /home/koyomi/lanraragi koyomi
+chown -R koyomi /home/koyomi/lanraragi
