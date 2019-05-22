b5742174faf614684338e5a5fdd0dd5720ced421
difegue <sugoi@cock.li>
2019-05-22 23:26:20 +0200

    Futureproof karen URL

--- .github/action-wslbuild/entrypoint.sh
+++ .github/action-wslbuild/entrypoint.sh
@@ -17,7 +17,7 @@ mv package.tar win_package
 
 # Download LxRunOffline and Karen
 wget https://github.com/DDoSolitary/LxRunOffline/releases/download/v3.3.3/LxRunOffline-v3.3.3.zip
-wget https://github.com/Difegue/Karen/releases/download/v1.0/Karen-v1.zip
+wget https://github.com/Difegue/Karen/releases/latest/download/Karen.zip
 
 # Unzip them to installer folder under the desired names
 unzip -d win_package/LxRunOffline LxRunOffline-v3.3.3.zip 
