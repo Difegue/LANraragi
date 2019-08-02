7fe1c718e9e4a6265c208051bd6a0e986e638197
CirnoT <1447794+CirnoT@users.noreply.github.com>
2019-08-02 11:37:11 +0200

    Add back support for using ExHentai

--- lib/LANraragi/Plugin/EHentai.pm
+++ lib/LANraragi/Plugin/EHentai.pm
@@ -24,15 +24,16 @@ sub plugin_info {
         author      => "Difegue",
         version     => "2.2",
         description => "Searches g.e-hentai for tags matching your archive. 
-If you have an account that can access fjorded content, adding the credentials here will make more archives available for parsing.",
+If you have an account that can access fjorded content or exhentai, adding the credentials here will make more archives available for parsing.",
         icon        => "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAUCAYAAACNiR0NAAAABmJLR0QA/wD/AP+gvaeTAAAACXBI\nWXMAAAsTAAALEwEAmpwYAAAAB3RJTUUH4wYBFg0JvyFIYgAAAB1pVFh0Q29tbWVudAAAAAAAQ3Jl\nYXRlZCB3aXRoIEdJTVBkLmUHAAAEo0lEQVQ4y02UPWhT7RvGf8/5yMkxMU2NKaYIFtKAHxWloYNU\ncRDeQTsUFPwAFwUHByu4ODq4Oghdiri8UIrooCC0Lx01ONSKfYOioi1WpWmaxtTm5PTkfNzv0H/D\n/9oeePjdPNd13Y8aHR2VR48eEUURpmmiaRqmaXbOAK7r4vs+IsLk5CSTk5P4vo9hGIgIsViMra0t\nCoUCRi6XY8+ePVSrVTRN61yybZuXL1/y7t078vk8mUyGvXv3cuLECWZnZ1lbW6PdbpNIJHAcB8uy\nePr0KYZlWTSbTRKJBLquo5TCMAwmJia4f/8+Sini8Ti1Wo0oikin09i2TbPZJJPJUK/XefDgAefO\nnWNlZQVD0zSUUvi+TxAE6LqOrut8/fqVTCaDbdvkcjk0TSOdTrOysoLrujiOw+bmJmEYMjAwQLVa\nJZVKYXR1ddFut/F9H9M0MU0T3/dZXV3FdV36+/vp7u7m6NGj7Nq1i0qlwuLiIqVSib6+Pubn5wGw\nbZtYLIaxMymVSuH7PpZlEUURSina7TZBEOD7Pp8/fyYMQ3zfZ25ujv3795NOp3n48CE9PT3ouk4Q\nBBi/fv3Ctm0cx6Grq4utrS26u7sREQzDIIoifv78SU9PD5VKhTAMGRoaYnV1leHhYa5evUoQBIRh\niIigiQhRFKHrOs1mE9u2iaKIkydPYhgGAKZp8v79e+LxOPl8Htd1uXbtGrdv3yYMQ3ZyAODFixeb\nrVZLvn//Lq7rSqVSkfX1dREROXz4sBw/flyUUjI6OipXrlyRQ4cOSbPZlCiKxHVdCcNQHMcRz/PE\ndV0BGL53756sra1JrVaT9fV1cRxHRESGhoakr69PUqmUvHr1SsrlsuzI931ptVriuq78+fNHPM+T\nVqslhoikjh075p09e9ba6aKu6/T39zM4OMjS0hIzMzM0Gg12794N0LEIwPd9YrEYrusShiEK4Nmz\nZ41yudyVy+XI5/MMDAyQzWap1+tks1lEhIWFBQqFArZto5QiCAJc1+14t7m5STweRwOo1WoSBAEj\nIyMUi0WSySQiQiqV6lRoYWGhY3673e7sfRAEiAjZbBbHcbaBb9++5cCBA2SzWZLJJLZt43kesViM\nHX379g1d1wnDsNNVEQEgCAIajQZ3797dBi4tLWGaJq7rYpompVKJmZkZ2u12B3j58mWUUmiahoiw\nsbFBEASdD2VsbIwnT55gACil+PHjB7Ozs0xPT/P7929u3ryJZVmEYUgYhhQKBZRSiAie52EYBkop\nLMvi8ePHTE1NUSwWt0OZn5/3hoeHzRs3bqhcLseXL1+YmJjowGzbRtO07RT/F8jO09+8ecP58+dJ\nJBKcPn0abW5uThWLRevOnTv/Li4u8vr1a3p7e9E0jXg8zsePHymVSnz69Kmzr7quY9s2U1NTXLp0\nCc/zOHLkCPv27UPxf6rX63+NjIz8IyKMj48zPT3NwYMHGRwcpLe3FwARodVqcf36dS5evMj4+DhB\nEHDmzBkymQz6DqxSqZDNZr8tLy//DYzdunWL5eVlqtUqHz58IJVKkUwmaTQalMtlLly4gIjw/Plz\nTp06RT6fZ2Njg/8AqMV7tO07rnsAAAAASUVORK5CYII=",
         parameters  => [
             {type => "int",    desc =>  "ipb_member_id cookie"},
             {type => "string", desc =>  "ipb_pass_hash cookie"},
-            {type => "string", desc =>  "star cookie"},
+            {type => "string", desc =>  "star cookie (required to view fjorded content without exhentai)"},
             {type => "string", desc =>  "Default language to use in searches"},
             {type => "bool",   desc =>  "Save archive title"},
-            {type => "bool",   desc =>  "Fetch using thumbnail first"}
+            {type => "bool",   desc =>  "Fetch using thumbnail first"},
+            {type => "bool",   desc =>  "Use ExHentai (enable if you can't view fjorded content on main site - only certain users can)"}
         ],
         oneshot_arg => "E-H Gallery URL (Will attach tags matching this exact gallery to your archive)"
     );
@@ -44,14 +45,15 @@ If you have an account that can access fjorded content, adding the credentials h
 # Returns the UA object created, alongside the domainname it can access. (e-h)
 sub get_user_agent {
 
-    my ($ipb_member_id, $ipb_pass_hash, $star) = @_;
+    my ($ipb_member_id, $ipb_pass_hash, $star, $enablepanda) = @_;
 
     my $logger = LANraragi::Utils::Generic::get_logger( "E-Hentai", "plugins" );
     my $domain = "https://e-hentai.org";
+    $domain = "https://exhentai.org" if $enablepanda;
     my $ua = Mojo::UserAgent->new;
 
     if ($ipb_member_id ne "" && $ipb_pass_hash ne "") {
-        $logger->info( "Cookies provided ($ipb_member_id $ipb_pass_hash $star)!");
+        $logger->info( "Cookies provided [$domain] ($ipb_member_id $ipb_pass_hash $star)!");
 
         #Setup the needed cookies with the e-hentai domain
         #They should translate to exhentai cookies with the igneous value generated
@@ -59,7 +61,7 @@ sub get_user_agent {
             Mojo::Cookie::Response->new(
                 name   => 'ipb_member_id',
                 value  => $ipb_member_id,
-                domain => 'e-hentai.org',
+                domain => ($enablepanda ? 'exhentai.org' : 'e-hentai.org'),
                 path   => '/'
             )
         );
@@ -68,7 +70,7 @@ sub get_user_agent {
             Mojo::Cookie::Response->new(
                 name   => 'ipb_pass_hash',
                 value  => $ipb_pass_hash,
-                domain => 'e-hentai.org',
+                domain => ($enablepanda ? 'exhentai.org' : 'e-hentai.org'),
                 path   => '/'
             )
         );
@@ -77,7 +79,7 @@ sub get_user_agent {
             Mojo::Cookie::Response->new(
                 name   => 'star',
                 value  => $star,
-                domain => 'e-hentai.org',
+                domain => ($enablepanda ? 'exhentai.org' : 'e-hentai.org'),
                 path   => '/'
             )
         );
@@ -103,7 +105,7 @@ sub get_tags {
     # the filesystem path, and the custom arguments at the end if available.
     shift;
     my ( $title, $tags, $thumbhash, $file, $oneshotarg,
-         $ipb_member_id, $ipb_pass_hash, $star, $lang, $savetitle, $usethumbs) = @_;
+         $ipb_member_id, $ipb_pass_hash, $star, $lang, $savetitle, $usethumbs, $enablepanda) = @_;
 
     # Use the logger to output status - they'll be passed to a specialized logfile and written to STDOUT.
     my $logger = LANraragi::Utils::Generic::get_logger( "E-Hentai", "plugins" );
@@ -111,7 +113,7 @@ sub get_tags {
     # Work your magic here - You can create subroutines below to organize the code better
     my $gID    = "";
     my $gToken = "";
-    my ( $ua, $domain ) = get_user_agent($ipb_member_id, $ipb_pass_hash, $star);
+    my ( $ua, $domain ) = get_user_agent($ipb_member_id, $ipb_pass_hash, $star, $enablepanda);
 
     # Quick regex to get the E-H archive ids from the provided url.
     if ( $oneshotarg =~ /.*\/g\/([0-9]*)\/([0-z]*)\/*.*/ ) {
