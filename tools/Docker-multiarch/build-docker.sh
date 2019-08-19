f1da8f41255a44555c03386f312176ea2ca2e503
difegue <sugoi@cock.li>
2019-08-19 02:35:19 +0200

    Parallelize multiarch builds because fuck waiting 40 minutes

--- tools/Docker-multiarch/build-docker.sh
+++ tools/Docker-multiarch/build-docker.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 # Script for building multi-arch LRR images using QEMU when necessary. 
 # Shamelessly lifted off https://lobradov.github.io/Building-docker-multiarch-images
+# example: ./build-docker.sh x86_64 nightly
 
 echo "Enabling QEMU builds for this Docker host"
 docker run --rm --privileged multiarch/qemu-user-static:register
@@ -11,21 +12,20 @@ for target_arch in aarch64 arm x86_64; do
   tar -xvf x86_64_qemu-${target_arch}-static.tar.gz
 done
 
-echo "Building per-architecture Dockerfiles and images for $1"
-for docker_arch in amd64 arm32v6 arm64v8; do
-  case ${docker_arch} in
-    amd64   ) qemu_arch="x86_64" ;;
-    arm32v6 ) qemu_arch="arm" ;;
-    arm64v8 ) qemu_arch="aarch64" ;;    
-  esac
-  cp ./tools/Docker-multiarch/Dockerfile Dockerfile.${docker_arch}
-  sed -i "s|__BASEIMAGE_ARCH__|${docker_arch}|g" Dockerfile.${docker_arch}
-  sed -i "s|__QEMU_ARCH__|${qemu_arch}|g" Dockerfile.${docker_arch}
-  if [ ${docker_arch} = 'amd64' ]; then
-    sed -i "/__CROSS_/d" Dockerfile.${docker_arch}
-  else
-    sed -i "s/__CROSS_//g" Dockerfile.${docker_arch}
-  fi
+echo "Building Dockerfile and image for $1, tag $2"
 
-  docker build -f Dockerfile.${docker_arch} -t difegue/lanraragi:$1-${docker_arch} .
-done
+case $1 in
+  amd64   ) qemu_arch="x86_64" ;;
+  arm32v6 ) qemu_arch="arm" ;;
+  arm64v8 ) qemu_arch="aarch64" ;;    
+esac
+cp ./tools/Docker-multiarch/Dockerfile Dockerfile.$1
+sed -i "s|__BASEIMAGE_ARCH__|$1|g" Dockerfile.$1
+sed -i "s|__QEMU_ARCH__|${qemu_arch}|g" Dockerfile.$1
+if [ $1 = 'amd64' ]; then
+  sed -i "/__CROSS_/d" Dockerfile.$1
+else
+  sed -i "s/__CROSS_//g" Dockerfile.$1
+fi
+
+docker build -f Dockerfile.$1 -t difegue/lanraragi:$2-$1 .
