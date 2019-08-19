a90f95b4bee8603a5fc21a29bff7c4682fdd6dd7
difegue <sugoi@cock.li>
2019-08-19 23:33:02 +0200

    Turns out I was using the wrong experimental all this time smh

--- tools/Docker-multiarch/push-docker.sh
+++ tools/Docker-multiarch/push-docker.sh
@@ -2,10 +2,6 @@
 # Use in conjunction with build-docker.sh
 # If not logged to a Docker repo this will obviously fail."
 
-echo "Starting experimental Docker"
-sudo service docker stop
-sudo dockerd --experimental &
-
 echo "Pushing multi-arch manifest"
 docker manifest create difegue/lanraragi:$1 difegue/lanraragi:$1-amd64 difegue/lanraragi:$1-arm32v6 difegue/lanraragi:$1-arm64v8 
 docker manifest annotate difegue/lanraragi:$1 difegue/lanraragi:$1-arm32v6 --os linux --arch arm
