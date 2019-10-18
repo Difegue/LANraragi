f035da042e1480da792315f26e93fc3e1acff317
difegue <sugoi@cock.li>
2019-10-18 20:14:18 +0200

    redo the whole qemu thing by throwing the binaries in /tmp

--- tools/Docker-multiarch/build-docker.sh
+++ tools/Docker-multiarch/build-docker.sh
@@ -8,8 +8,8 @@ docker run --rm --privileged multiarch/qemu-user-static:register
 
 echo "Downloading QEMU binaries"
 for target_arch in aarch64 arm x86_64; do
-  wget -N https://github.com/multiarch/qemu-user-static/releases/download/v2.9.1-1/x86_64_qemu-${target_arch}-static.tar.gz
-  tar -xvf x86_64_qemu-${target_arch}-static.tar.gz
+  wget -N https://github.com/multiarch/qemu-user-static/releases/download/v2.9.1-1/x86_64_qemu-${target_arch}-static.tar.gz -O /tmp/x86_64_qemu-${target_arch}-static.tar.gz
+  tar -xvf /tmp/x86_64_qemu-${target_arch}-static.tar.gz -C /tmp
 done
 
 echo "Building Dockerfile and image for $1, tag $2"
@@ -19,15 +19,17 @@ case $1 in
   arm32v6 ) qemu_arch="arm" ;;
   arm64v8 ) qemu_arch="aarch64" ;;    
 esac
-cp ./tools/Docker-multiarch/Dockerfile Dockerfile.$1
-sed -i "s|__BASEIMAGE_ARCH__|$1|g" Dockerfile.$1
-sed -i "s|__QEMU_ARCH__|${qemu_arch}|g" Dockerfile.$1
+
+cp ./tools/Docker-multiarch/Dockerfile ./tools/Docker-multiarch/Dockerfile.$1
+sed -i "s|__BASEIMAGE_ARCH__|$1|g" ./tools/Docker-multiarch/Dockerfile.$1
+sed -i "s|__QEMU_ARCH__|${qemu_arch}|g" ./tools/Docker-multiarch/Dockerfile.$1
 if [ $1 = 'amd64' ]; then
-  sed -i "/__CROSS_/d" Dockerfile.$1
+  sed -i "/__CROSS_/d" ./tools/Docker-multiarch/Dockerfile.$1
 else
-  sed -i "s/__CROSS_//g" Dockerfile.$1
+  sed -i "s/__CROSS_//g" ./tools/Docker-multiarch/Dockerfile.$1
+  cp /tmp/qemu-${qemu_arch}-static ./qemu-${qemu_arch}-static
 fi
 
-docker build -f Dockerfile.$1 -t difegue/lanraragi:$2-$1 .
+docker build -f ./tools/Docker-multiarch/Dockerfile.$1 -t difegue/lanraragi:$2-$1 .
 echo "Image built, trying a push"
 docker push difegue/lanraragi:$2-$1
