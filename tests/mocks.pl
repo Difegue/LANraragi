4359686022cd7bcb198e484c03aac2a3b1fde36c
difegue <sugoi@cock.li>
2020-04-19 16:32:16 +0200

    (#271) Fix the search mocks relying on README.md (instead it's package.json lmao)

--- tests/mocks.pl
+++ tests/mocks.pl
@@ -24,6 +24,7 @@ sub setup_eze_mock {
 sub setup_redis_mock {
 
     # DataModel for searches
+    # files are set to package.json since the search engine checks for file existence and I ain't about to mock perl's -e call
     # Switch devmode to 1 for debug output in test
     my %datamodel = %{decode_json qq(
         {
@@ -35,37 +36,37 @@ sub setup_redis_mock {
             "isnew": "none",
             "tags": "character:segata sanshiro",
             "title": "Saturn Backup Cartridge - Japanese Manual",
-            "file": "README.md"
+            "file": "package.json"
         },
         "e69e43e1355267f7d32a4f9b7f2fe108d2401ebg": {
             "isnew": "none",
             "tags": "character:segata",
             "title": "Saturn Backup Cartridge - American Manual",
-            "file": "README.md"
+            "file": "package.json"
         },
         "e4c422fd10943dc169e3489a38cdbf57101a5f7e": {
             "isnew": "true",
             "tags": "parody: jojo's bizarre adventure",
             "title": "Rohan Kishibe goes to Gucci",
-            "file": "README.md"
+            "file": "package.json"
         },
         "4857fd2e7c00db8b0af0337b94055d8445118630": {
             "isnew": "none",
             "tags": "artist:shirow masamune",
             "title": "Ghost in the Shell 1.5 - Human-Error Processor vol01ch01",
-            "file": "README.md"
+            "file": "package.json"
         },
         "2810d5e0a8d027ecefebca6237031a0fa7b91eb3": {
             "isnew": "none",
             "tags": "parody:fate grand order,  character:abigail williams,  character:artoria pendragon alter,  character:asterios,  character:ereshkigal,  character:gilgamesh,  character:hans christian andersen,  character:hassan of serenity,  character:hector,  character:helena blavatsky,  character:irisviel von einzbern,  character:jeanne alter,  character:jeanne darc,  character:kiara sessyoin,  character:kiyohime,  character:lancer,  character:martha,  character:minamoto no raikou,  character:mochizuki chiyome,  character:mordred pendragon,  character:nitocris,  character:oda nobunaga,  character:osakabehime,  character:penthesilea,  character:queen of sheba,  character:rin tosaka,  character:saber,  character:sakata kintoki,  character:scheherazade,  character:sherlock holmes,  character:suzuka gozen,  character:tamamo no mae,  character:ushiwakamaru,  character:waver velvet,  character:xuanzang,  character:zhuge liang,  group:wadamemo,  artist:wada rco,  artbook,  full color",
             "title": "Fate GO MEMO 2",
-            "file": "README.md"
+            "file": "package.json"
         },
         "28697b96f0ac5858be2614ed10ca47742c9522fd": {
             "isnew": "none",
             "tags": "parody:fate grand order,  group:wadamemo,  artist:wada rco,  artbook,  full color",
             "title": "Fate GO MEMO",
-            "file": "README.md"
+            "file": "package.json"
         }
     })};
 
