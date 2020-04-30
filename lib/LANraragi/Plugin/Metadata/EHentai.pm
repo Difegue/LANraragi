bc064175281b22e9259b935ccfb989e9e864add2
difegue <sugoi@cock.li>
2020-04-30 21:54:01 +0200

    Fix argument order in EH.pm as that'd break compatibility

--- lib/LANraragi/Plugin/Metadata/EHentai.pm
+++ lib/LANraragi/Plugin/Metadata/EHentai.pm
@@ -23,20 +23,22 @@ sub plugin_info {
         type        => "metadata",
         namespace   => "ehplugin",
         login_from  => "ehlogin",
-        author      => "Difegue",
-        version     => "2.3",
+        author      => "Difegue and others",
+        version     => "2.5",
         description => "Searches g.e-hentai for tags matching your archive.",
         icon =>
           "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAUCAYAAACNiR0NAAAABmJLR0QA/wD/AP+gvaeTAAAACXBI\nWXMAAAsTAAALEwEAmpwYAAAAB3RJTUUH4wYBFg0JvyFIYgAAAB1pVFh0Q29tbWVudAAAAAAAQ3Jl\nYXRlZCB3aXRoIEdJTVBkLmUHAAAEo0lEQVQ4y02UPWhT7RvGf8/5yMkxMU2NKaYIFtKAHxWloYNU\ncRDeQTsUFPwAFwUHByu4ODq4Oghdiri8UIrooCC0Lx01ONSKfYOioi1WpWmaxtTm5PTkfNzv0H/D\n/9oeePjdPNd13Y8aHR2VR48eEUURpmmiaRqmaXbOAK7r4vs+IsLk5CSTk5P4vo9hGIgIsViMra0t\nCoUCRi6XY8+ePVSrVTRN61yybZuXL1/y7t078vk8mUyGvXv3cuLECWZnZ1lbW6PdbpNIJHAcB8uy\nePr0KYZlWTSbTRKJBLquo5TCMAwmJia4f/8+Sini8Ti1Wo0oikin09i2TbPZJJPJUK/XefDgAefO\nnWNlZQVD0zSUUvi+TxAE6LqOrut8/fqVTCaDbdvkcjk0TSOdTrOysoLrujiOw+bmJmEYMjAwQLVa\nJZVKYXR1ddFut/F9H9M0MU0T3/dZXV3FdV36+/vp7u7m6NGj7Nq1i0qlwuLiIqVSib6+Pubn5wGw\nbZtYLIaxMymVSuH7PpZlEUURSina7TZBEOD7Pp8/fyYMQ3zfZ25ujv3795NOp3n48CE9PT3ouk4Q\nBBi/fv3Ctm0cx6Grq4utrS26u7sREQzDIIoifv78SU9PD5VKhTAMGRoaYnV1leHhYa5evUoQBIRh\niIigiQhRFKHrOs1mE9u2iaKIkydPYhgGAKZp8v79e+LxOPl8Htd1uXbtGrdv3yYMQ3ZyAODFixeb\nrVZLvn//Lq7rSqVSkfX1dREROXz4sBw/flyUUjI6OipXrlyRQ4cOSbPZlCiKxHVdCcNQHMcRz/PE\ndV0BGL53756sra1JrVaT9fV1cRxHRESGhoakr69PUqmUvHr1SsrlsuzI931ptVriuq78+fNHPM+T\nVqslhoikjh075p09e9ba6aKu6/T39zM4OMjS0hIzMzM0Gg12794N0LEIwPd9YrEYrusShiEK4Nmz\nZ41yudyVy+XI5/MMDAyQzWap1+tks1lEhIWFBQqFArZto5QiCAJc1+14t7m5STweRwOo1WoSBAEj\nIyMUi0WSySQiQiqV6lRoYWGhY3673e7sfRAEiAjZbBbHcbaBb9++5cCBA2SzWZLJJLZt43kesViM\nHX379g1d1wnDsNNVEQEgCAIajQZ3797dBi4tLWGaJq7rYpompVKJmZkZ2u12B3j58mWUUmiahoiw\nsbFBEASdD2VsbIwnT55gACil+PHjB7Ozs0xPT/P7929u3ryJZVmEYUgYhhQKBZRSiAie52EYBkop\nLMvi8ePHTE1NUSwWt0OZn5/3hoeHzRs3bqhcLseXL1+YmJjowGzbRtO07RT/F8jO09+8ecP58+dJ\nJBKcPn0abW5uThWLRevOnTv/Li4u8vr1a3p7e9E0jXg8zsePHymVSnz69Kmzr7quY9s2U1NTXLp0\nCc/zOHLkCPv27UPxf6rX63+NjIz8IyKMj48zPT3NwYMHGRwcpLe3FwARodVqcf36dS5evMj4+DhB\nEHDmzBkymQz6DqxSqZDNZr8tLy//DYzdunWL5eVlqtUqHz58IJVKkUwmaTQalMtlLly4gIjw/Plz\nTp06RT6fZ2Njg/8AqMV7tO07rnsAAAAASUVORK5CYII=",
         parameters => [
             { type => "string", desc => "Forced language to use in searches" },
             { type => "bool",   desc => "Save archive title" },
-            { type => "bool",   desc => "Save the original Japanese title when available instead of the English or "
-              . "romanised title" },
-            { type => "bool",   desc => "Fetch additional timestamp (time posted) and uploader metadata" },
-            { type => "bool",   desc => "Fetch using cover thumbnail instead of archive title" },
-            { type => "bool",   desc => "Search expunged galleries as well" },
-            { type => "bool",   desc => "Use ExHentai (enable to search for fjorded content without star cookie)" }
+            { type => "bool",   desc => "Fetch using thumbnail first (falls back to title)" },
+            { type => "bool",   desc => "Use ExHentai (enable to search for fjorded content without star cookie)" },
+            {   type => "bool",
+                desc => "Save the original Japanese title when available instead of the English or " . "romanised title"
+            },
+            { type => "bool", desc => "Fetch additional timestamp (time posted) and uploader metadata" },
+            { type => "bool", desc => "Search expunged galleries as well" },
+
         ],
         oneshot_arg => "E-H Gallery URL (Will attach tags matching this exact gallery to your archive)"
     );
@@ -48,7 +50,7 @@ sub get_tags {
 
     shift;
     my $lrr_info = shift;    # Global info hash
-    my ( $lang, $savetitle, $jpntitle, $additionaltags, $usethumbs, $expunged, $enablepanda ) = @_;    # Plugin parameters
+    my ( $lang, $savetitle, $usethumbs, $enablepanda, $jpntitle, $additionaltags, $expunged ) = @_;    # Plugin parameters
 
     # Use the logger to output status - they'll be passed to a specialized logfile and written to STDOUT.
     my $logger = get_logger( "E-Hentai", "plugins" );
@@ -132,7 +134,7 @@ sub lookup_gallery {
           . "&fs_covers=1&fs_similar=1";
 
         #Include expunged galleries in the search if the option is enabled.
-        if ( $expunged ) {
+        if ($expunged) {
             $URL = $URL . "&fs_exp=1";
         }
 
@@ -158,7 +160,7 @@ sub lookup_gallery {
       . uri_escape_utf8( qw(") . $title . qw(") );
 
     #Include expunged galleries in the search if the option is enabled.
-    if ( $expunged ) {
+    if ($expunged) {
         $URL = $URL . "&f_sh=on";
     }
 
@@ -248,16 +250,16 @@ sub get_tags_from_EH {
 
         my $data    = $jsonresponse->{"gmetadata"};
         my $tags    = @$data[0]->{"tags"};
-        my $ehtitle = @$data[0]->{($jpntitle ? "title_jpn" : "title")};
+        my $ehtitle = @$data[0]->{ ( $jpntitle ? "title_jpn" : "title" ) };
         if ( $ehtitle eq "" && $jpntitle ) {
             $ehtitle = @$data[0]->{"title"};
         }
-        my $ehcat   = lc @$data[0]->{"category"};
+        my $ehcat = lc @$data[0]->{"category"};
 
         my $ehtags = join( ", ", @$tags );
         $ehtags = $ehtags . ", category:" . $ehcat;
-        if ( $additionaltags ) {
-            my $ehuploader = @$data[0]->{"uploader"};
+        if ($additionaltags) {
+            my $ehuploader  = @$data[0]->{"uploader"};
             my $ehtimestamp = @$data[0]->{"posted"};
             $ehtags = $ehtags . ", uploader:" . $ehuploader . ", timestamp:" . $ehtimestamp;
         }
