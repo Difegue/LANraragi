79d1363d86c18b7ccdcb7ffc2642c92f852143ed
difegue <sugoi@cock.li>
2020-05-08 01:30:47 +0200

    fix entrypoint still using the old location of supervisord.conf

--- tools/build/docker/entrypoint.sh
+++ tools/build/docker/entrypoint.sh
@@ -57,7 +57,7 @@ fi
 if [ $USER_ID -eq 0 ] && [ $GROUP_ID -eq 0 ] 
 then
     echo "UID and GID set to 0, running as root. You've been warned!"
-    exec supervisord --nodaemon --configuration ./tools/build/docker/supervisord.conf
+    exec supervisord --nodaemon --configuration ./tools/supervisord.conf
 else   
-    exec su-exec koyomi supervisord --nodaemon --configuration ./tools/build/docker/supervisord.conf
+    exec su-exec koyomi supervisord --nodaemon --configuration ./tools/supervisord.conf
 fi
