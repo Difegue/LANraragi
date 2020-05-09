e7e4f66339cc76a32e2e13b7f951f58ba6899f18
Alex <alexwh@users.noreply.github.com>
2020-05-09 20:39:16 +0100

    Switch from supervisord to s6-overlay (#293)
    * initial s6 transition
    
    * move install-everything.sh to Dockerfile
    
    changing anything in tools folder causes whole build to retrigger, avoid
    that
    
    * add .dockerignore excluding documentation
    
    previous removed in install-everything.sh
    
    * move wsl.conf to docker folder
    
    it gets excluded by the dockerignore file otherwise, and the copy
    technically happens in the docker layer, so
    
    * remove redis overcommit_memory messages
    
    redis tells us this on startup itself
    
    * set S6_KEEP_ENV
    
    we make extensive use of envvars in lrr and the setup scripts
    there shouldn't be anything sensitive, so pass them to all processes
    with this, rather than restrictively using `with-contenv`
    
    * set uid and gid for services
    
    * terminate lrr also if the service fails
    
    * set s6 stage 2 fail early and kill time opts
    
    * remove manual inotify2 call
    
    https://github.com/Difegue/LANraragi/pull/293#discussion_r421463387
    
    * revert install-everything.sh consolidation
    
    * remove chmod +x and set exec directly on file
    
    * fix old comment
    
    * add install-everything.sh to tools
    
    * Update tools/build/docker/s6/services.d/redis/run
    
    Co-authored-by: Difegue <sugoi@cock.li>
    
    * readd redis OVERCOMMIT warning
    
    * move redis.conf back to tools/build/docker
    
    Co-authored-by: koma-cute <koma-cute@users.noreply.github.com>
    Co-authored-by: koma-cute <39638109+koma-cute@users.noreply.github.com>
    Co-authored-by: Difegue <sugoi@cock.li>

--- tools/build/docker/install-everything.sh
+++ tools/build/docker/install-everything.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 
-#Just do everything 
-apk update 
-apk add perl perl-io-socket-ssl perl-dev redis libarchive-dev libbz2 openssl-dev zlib-dev 
+#Just do everything
+apk update
+apk add perl perl-io-socket-ssl perl-dev redis libarchive-dev libbz2 openssl-dev zlib-dev
 apk add imagemagick imagemagick-perlmagick libwebp-tools
 apk add g++ make pkgconf gnupg wget curl nodejs nodejs-npm
-apk add supervisor su-exec shadow
+apk add shadow s6 s6-portable-utils
 
 #Hey it's cpanm
-curl -L https://cpanmin.us | perl - App::cpanminus 
+curl -L https://cpanmin.us | perl - App::cpanminus
 
 #Install the LRR dependencies proper
 cd tools && cpanm --notest --installdeps . -M https://cpan.metacpan.org && cd ..
-npm run lanraragi-installer install-front 
+npm run lanraragi-installer install-front
 
 #Cleanup to lighten the image
 apk del perl-dev g++ make gnupg wget curl nodejs nodejs-npm openssl-dev
-rm -rf /root/.cpanm/* /root/.npm/* /usr/local/share/man/* node_modules
+rm -rf /root/.cpanm/* /root/.npm/ /usr/local/share/man/* node_modules /var/cache/apk/*
