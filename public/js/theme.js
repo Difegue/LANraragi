7ee0c4beb228ebacb8c7e416815c1f8c9551eb34
difegue <sugoi@cock.li>
2021-03-17 15:45:21 +0100

    Cleanup previous svg logo code

--- public/js/theme.js
+++ public/js/theme.js
@@ -64,39 +64,6 @@ function switch_style(css_title) {
 
 }
 
-function replace_img_svg() {
-  // Replace svgs in <img> tags by the actual svg xml 
-  // This allows use of its internal "logo" classes to change the fill color
-  jQuery('img.svg').each(function () {
-    var $img = jQuery(this);
-    var imgID = $img.attr('id');
-    var imgClass = $img.attr('class');
-    var imgURL = $img.attr('src');
-
-    jQuery.get(imgURL, function (data) {
-      // Get the SVG tag, ignore the rest
-      var $svg = jQuery(data).find('svg');
-
-      // Add replaced image's ID to the new SVG
-      if (typeof imgID !== 'undefined') {
-        $svg = $svg.attr('id', imgID);
-      }
-      // Add replaced image's classes to the new SVG
-      if (typeof imgClass !== 'undefined') {
-        $svg = $svg.attr('class', imgClass + ' replaced-svg');
-      }
-
-      // Remove any invalid XML tags as per http://validator.w3.org
-      $svg = $svg.removeAttr('xmlns:a');
-
-      // Replace image with new SVG
-      $img.replaceWith($svg);
-
-    }, 'xml');
-
-  });
-}
-
 function set_style_from_storage() {
 
   var css_title = localStorage.customCSS;
