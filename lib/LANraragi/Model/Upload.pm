36876ce8fd1bfd0c8cf3bebf52c53cc2f2213835
koma-cute <39638109+koma-cute@users.noreply.github.com>
2021-05-05 02:12:50 +0100

    limit filename length

--- lib/LANraragi/Model/Upload.pm
+++ lib/LANraragi/Model/Upload.pm
@@ -172,6 +172,12 @@ sub download_url {
     
     # remove invalid Windows chars
     $filename =~ s@[\\/:"*?<>|]+@@g;
+    
+    my ($fn, $path, $ext) = fileparse($filename, qr/\.[^.]*/);
+    # don't allow the main filename to exceed 255 chars after accounting
+    # for extension and .upload prefix used by `handle_incoming_file`
+    $filename = substr $fn, 0, 254-length($ext)-length(".upload");
+    $filename = $filename . $ext;
     $logger->debug("Filename post clean: $filename");
     $tx->result->save_to("$tempdir\/$filename");
 
