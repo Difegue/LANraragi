c5cb26c793487d19300ed2816536530776c63b63
koma-cute <39638109+koma-cute@users.noreply.github.com>
2021-05-05 22:54:17 +0100

    Fix eh downloader autostart behavior (#465)
    * fix eh downloader autostart behavior
    
    no query leads to a webpage with the URL
    ?autostart=1 is the same thing but with a js trigger to redirect to ?start=1
    which is the actual file, so always set that instead
    
    * add use URI
    
    oopsie hehe
    
    * Update lib/LANraragi/Plugin/Download/EHentai.pm
    
    Co-authored-by: Difegue <sugoi@cock.li>
    
    * add URI to cpanfile
    
    Co-authored-by: Difegue <sugoi@cock.li>

--- lib/LANraragi/Plugin/Download/EHentai.pm
+++ lib/LANraragi/Plugin/Download/EHentai.pm
@@ -4,6 +4,7 @@ use strict;
 use warnings;
 no warnings 'uninitialized';
 
+use URI;
 use Mojo::UserAgent;
 
 use LANraragi::Utils::Logging qw(get_logger);
@@ -96,11 +97,11 @@ sub provide_url {
     $content = $response->body;
     $logger->debug("/archiver.php result: $content");
 
-    my $finalURL = "";
+    my $finalURL = URI->new();
     eval {
         # Parse that to get the final URL
         if ( $content =~ /.*document.location = "(.*)".*/gim ) {
-            $finalURL = $1;
+            $finalURL = URI->new($1);
             $logger->info("Final URL obtained: $finalURL");
         }
     };
@@ -109,10 +110,8 @@ sub provide_url {
         return ( error => "Couldn't proceed with an original size download: <pre>$content</pre>" );
     }
 
-    # If the URL doesn't already have autostart, append start=1 to get an URL that automatically triggers the download.
-    unless ( $finalURL =~ /.*autostart=1.*/gi ) {
-        $finalURL .= "?start=1";
-    }
+    # Set URL query parameters to ?start=1 to automatically trigger the download.
+    $finalURL->query("start=1");
 
     # All done!
     return ( download_url => $finalURL );
