0c8778ed2d1f1e38c9ef58a45aba94bdc8f695ed
difegue <sugoi@cock.li>
2022-04-15 15:07:45 +0200

    (#601) Fix timestamp tags replacing existing tags instead of simply adding to them

--- lib/LANraragi/Utils/Database.pm
+++ lib/LANraragi/Utils/Database.pm
@@ -60,15 +60,17 @@ sub add_timestamp_tag {
     if ( LANraragi::Model::Config->enable_dateadded eq "1" ) {
 
         $logger->debug("Adding timestamp tag...");
+        my $date;
 
         if ( LANraragi::Model::Config->use_lastmodified eq "1" ) {
             $logger->info("Using file date");
-            my $date = ( stat( $redis->hget( $id, "file" ) ) )[9];    #9 is the unix time stamp for date modified.
-            $redis->hset( $id, "tags", "date_added:$date" );
+            $date = ( stat( $redis->hget( $id, "file" ) ) )[9];    #9 is the unix time stamp for date modified.
         } else {
             $logger->info("Using current date");
-            $redis->hset( $id, "tags", "date_added:" . time() );
+            $date = time();
         }
+
+        add_tags( $id, "date_added:$date" );
     }
 }
 
