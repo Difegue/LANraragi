fb0b13947bcdcd79bb4223d941de5eba84bfdf9a
xiazeyu <i@xiaze.pro>
2022-05-01 23:06:40 +0800

    more robust compatability

--- public/js/init_toast.js
+++ public/js/init_toast.js
@@ -1,20 +1,17 @@
 jQuery(() => {
     window.toast = (c) => window.reactToastify.toast(window.React.createElement("div", { dangerouslySetInnerHTML: { __html: `<h2>${c.heading}</h2><p>${c.text}</p>` } }), {
-        toastId: c.toastId || undefined,
+        toastId: c.toastId,
         type: c.icon || c.type || "info",
         position: c.position || "top-left",
-        onOpen: c.onOpen || undefined,
-        onClose: c.onClose || undefined,
+        onOpen: c.onOpen,
+        onClose: c.onClose,
         autoClose: c.hideAfter || c.autoClose || ((c.icon || c.type) === "info" || (c.icon || c.type) === "success") ? 5000 : 7000,
-        closeButton: c.allowToastClose || c.closeButton || true,
-        // false at default
-        hideProgressBar: (typeof (c.loader) === "boolean" && !c.loader) || c.hideProgressBar,
-        pauseOnHover: c.pauseOnHover || true,
-        pauseOnFocusLoss: c.pauseOnFocusLoss || true,
-        // false at default
-        closeOnClick: c.closeOnClick || (c.icon || c.type) === "info" || (c.icon || c.type) === "success",
-        // false at default
-        draggable: c.draggable || (c.icon || c.type) === "info" || (c.icon || c.type) === "success",
+        closeButton: c.allowToastClose ?? c.closeButton ?? true,
+        hideProgressBar: (typeof (c.loader) === "boolean" && !c.loader) ?? c.hideProgressBar ?? false,
+        pauseOnHover: c.pauseOnHover ?? true,
+        pauseOnFocusLoss: c.pauseOnFocusLoss ?? true,
+        closeOnClick: c.closeOnClick ?? ((c.icon || c.type) === "info" || (c.icon || c.type) === "success") ?? false,
+        draggable: c.draggable ?? ((c.icon || c.type) === "info" || (c.icon || c.type) === "success") ?? false,
     });
     const toastDiv = document.createElement("div");
     document.body.appendChild(toastDiv);
