8283cda8c76cd6d59b82326dab5b1ec13f76d138
xiazeyu <i@xiaze.pro>
2022-05-01 20:34:36 +0800

    fix sourcemap not found in $.append

--- public/js/init_toast.js
+++ public/js/init_toast.js
@@ -1,4 +1,4 @@
-function initToast() {
+jQuery(() => {
     window.toast = (c) => window.reactToastify.toast(window.react.createElement("div", { dangerouslySetInnerHTML: { __html: `<h2>${c.heading}</h2>${c.text}` } }), {
         type: c.icon,
         position: c.position,
@@ -12,13 +12,4 @@ function initToast() {
     toastDiv.style.textAlign = "initial";
     window.react.render(
         window.react.createElement(window.reactToastify.ToastContainer, { theme: "colored" }, undefined), toastDiv);
-}
-
-$("head").append(`<link rel="stylesheet" type="text/css" href="/css/vendor/ReactToastify.min.css" />
-<script src="/js/vendor/preact.umd.js" type="text/JAVASCRIPT"></script>
-<script src="/js/vendor/hooks.umd.js" type="text/JAVASCRIPT"></script>
-<script src="/js/vendor/compat.umd.js" type="text/JAVASCRIPT"></script>
-<script>window.react = window.preactCompat;</script>
-<script src="/js/vendor/clsx.min.js" type="text/JAVASCRIPT"></script>
-<script src="/js/vendor/react-toastify.umd.js" type="text/JAVASCRIPT"></script>
-<script>jQuery(${initToast.toString()});</script>`);
+});
