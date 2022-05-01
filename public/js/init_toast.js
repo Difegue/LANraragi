451d7b36b836dab89e49d163bc5eba8f32f1845a
xiazeyu <i@xiaze.pro>
2022-05-01 19:49:43 +0800

    return values

--- public/js/init_toast.js
+++ public/js/init_toast.js
@@ -1,14 +1,12 @@
 function initToast() {
-    window.toast = (c) => {
-        window.reactToastify.toast(window.react.createElement("div", { dangerouslySetInnerHTML: { __html: `<h2>${c.heading}</h2>${c.text}` } }), {
-            type: c.icon,
-            position: c.position,
-            autoClose: c.hideAfter,
-            closeButton: c.allowToastClose,
-            limit: c.stack,
-            hideProgressBar: !c.loader,
-        });
-    };
+    window.toast = (c) => window.reactToastify.toast(window.react.createElement("div", { dangerouslySetInnerHTML: { __html: `<h2>${c.heading}</h2>${c.text}` } }), {
+        type: c.icon,
+        position: c.position,
+        autoClose: c.hideAfter,
+        closeButton: c.allowToastClose,
+        limit: c.stack,
+        hideProgressBar: !c.loader,
+    });
     const toastDiv = document.createElement("div");
     document.body.appendChild(toastDiv);
     toastDiv.style.textAlign = "initial";
