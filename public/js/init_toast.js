2e803e77f35804020b017a52dbde3568680b7d4c
xiazeyu <i@xiaze.pro>
2022-05-02 18:06:06 +0800

    custom toastify style

--- public/js/init_toast.js
+++ public/js/init_toast.js
@@ -1,11 +1,11 @@
 jQuery(() => {
-    window.toast = (c) => window.reactToastify.toast(window.React.createElement("div", { dangerouslySetInnerHTML: { __html: `<h2>${c.heading}</h2><p>${c.text}</p>` } }), {
+    window.toast = (c) => window.reactToastify.toast(window.React.createElement("div", { dangerouslySetInnerHTML: { __html: `${c.heading ? `<h2>${c.heading}</h2>` : ""}${c.text ?? ""}` } }), {
         toastId: c.toastId,
         type: c.icon || c.type || "info",
         position: c.position || "top-left",
         onOpen: c.onOpen,
         onClose: c.onClose,
-        autoClose: c.hideAfter || c.autoClose || ((c.icon || c.type) === "info" || (c.icon || c.type) === "success") ? 5000 : 7000,
+        autoClose: c.hideAfter ?? c.autoClose ?? (((c.icon || c.type) === "info" || (c.icon || c.type) === "success") ? 5000 : 7000),
         closeButton: c.allowToastClose ?? c.closeButton ?? true,
         hideProgressBar: (typeof (c.loader) === "boolean" && !c.loader) ?? c.hideProgressBar ?? false,
         pauseOnHover: c.pauseOnHover ?? true,
