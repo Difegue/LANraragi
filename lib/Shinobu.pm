d1f1a02043f3fa70f203b20905e5b7556b5307dd
uparrows <38988286+uparrows@users.noreply.github.com>
2022-06-02 01:23:49 +0800

    Fix the problem of non-ISO8859-1 decoding garbled
    Fix the problem of non-ISO8859-1 decoding garbled

--- lib/Shinobu.pm
+++ lib/Shinobu.pm
@@ -303,8 +303,9 @@ sub deleted_file_callback {
 
 sub add_new_file {
 
-    my ( $id, $file, $redis ) = @_;
-    $logger->info("Adding new file $file with ID $id");
+    my ( $id, $file, $redis,$outputname ) = @_;
+	$outputname = decode("utf8", $file);
+    $logger->info("Adding new file $outputname with ID $id");
 
     eval {
         LANraragi::Utils::Database::add_archive_to_redis( $id, $file, $redis );
