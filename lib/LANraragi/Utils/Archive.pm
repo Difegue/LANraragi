e72d7a604408cbb80b69ecacebe1041ce6b6d5c9
Asinine <15137035+Asinin3@users.noreply.github.com>
2023-01-21 20:41:03 +1030

    Fix regex

--- lib/LANraragi/Utils/Archive.pm
+++ lib/LANraragi/Utils/Archive.pm
@@ -248,9 +248,9 @@ sub get_filelist {
     @files = sort { &expand($a) cmp &expand($b) } @files;
 
     # Move front cover pages to the start of a gallery, and miscelanious pages such as translator credits to the end.
-    my @cover_pages      = grep { /cover\.[^\.]*/i } @files;
-    my @credit_pages     = grep { /^999999|^bumper|^end_card_save_file|notes\.[^\.]*|note\.[^\.]*|artist_info_store_links|credit|999nhnl\.|^group\./i } @files;
-    my @non_credit_pages = grep { !/^999999|^bumper|^end_card_save_file|notes\.[^\.]*|note\.[^\.]*|artist_info_store_links|credit|999nhnl\.|^group\./i } @files;
+    my @cover_pages      = grep { /cover\.[^\.]*$/i } @files;
+    my @credit_pages     = grep { /^999999|^bumper|^end_card_save_file|notes\.[^\.]*$|note\.[^\.]*$|artist_info_store_links|credit|999nhnl\.|^group\./i } @files;
+    my @non_credit_pages = grep { !/^999999|^bumper|^end_card_save_file|notes\.[^\.]*$|note\.[^\.]*$|artist_info_store_links|credit|999nhnl\.|^group\./i } @files;
     @files = ( @cover_pages, @non_credit_pages, @credit_pages );
 
     # Return files and sizes in a hashref
