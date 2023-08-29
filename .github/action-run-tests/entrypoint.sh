fe5622cdd888cf93d86c3295159189b4f4381562
Difegue <sugoi@cock.li>
2023-08-29 16:07:33 +0200

    forgot this parameter doesn't need dashes

--- .github/action-run-tests/entrypoint.sh
+++ .github/action-run-tests/entrypoint.sh
@@ -3,7 +3,7 @@
 echo "🎌 Running LRR Test Suite 🎌"
 
 # Install cpan deps in case some are missing
-perl ./tools/install.pl --install-back
+perl ./tools/install.pl install-back
 
 # Run the perl tests on the repo
 prove -r -l -v tests/
