1657daf229aa2f0997389cf080cc5187bb6ba23d
Difegue <sugoi@cock.li>
2023-11-07 21:27:00 +0100

    Add Redis4Win to Karen build

--- tools/build/windows/build.ps1
+++ tools/build/windows/build.ps1
@@ -11,8 +11,18 @@ $env:LRR_VERSION_NUM=$version
 # Use Docker image
 mv .\package\package.tar .\tools\build\windows\Karen\External\package.tar 
 
+# Download and unpack Redis
+cd .\tools\build\windows\Karen\External
+Invoke-WebRequest -Uri https://github.com/redis-windows/redis-windows/releases/download/7.0.14/Redis-7.0.14-Windows-x64.tar.gz -OutFile .\redis.tar.gz
+tar -xzf .\redis.tar.gz
+rm .\redis.tar.gz
+mv .\Redis-7.0.14-Windows-x64 .\Redis
+
+# Copy redis.conf to redis folder
+cp ..\..\..\docker\redis.conf .\Redis\redis.conf
+
 # Use Karen master
-cd .\tools\build\windows\Karen
+cd ..
 echo (Resolve-Path .\).Path
 nuget restore
 
