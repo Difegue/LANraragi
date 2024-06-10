adf7e77282b5766205b29e1a0f1d19923e0eeb96
Nixis198 <gager198@gmail.com>
2024-06-10 17:29:20 -0400

    Fixed #983 No more ":in this month." tags

--- lib/LANraragi/Plugin/Metadata/Fakku.pm
+++ lib/LANraragi/Plugin/Metadata/Fakku.pm
@@ -25,7 +25,7 @@ sub plugin_info {
         namespace   => "fakkumetadata",
         login_from  => "fakkulogin",
         author      => "Difegue, Nodja, Nixis198",
-        version     => "0.92",
+        version     => "0.93",
         description =>
           "Searches FAKKU for tags matching your archive. If you have an account, don't forget to enter the matching cookie in the login plugin to be able to access controversial content. <br/><br/>  
            <i class='fa fa-exclamation-circle'></i> <b>This plugin can and will return invalid results depending on what you're searching for!</b> <br/>The FAKKU search API isn't very precise and I recommend you use the Chaika.moe plugin when possible.",
@@ -200,6 +200,8 @@ sub get_tags_from_fakku {
 
         $value = trim($value);
         $value = trim_CRLF($value);
+
+        next if ( $value = " in  this month.");
 
         $logger->debug("Parsed row: $namespace");
         $logger->debug("Matching tag: $value");
