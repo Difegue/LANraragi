08e3d099607c703ecd1f32e699f712f14776b7f4
Efrain <efrondx@gmail.com>
2024-08-01 22:55:08 -0600

    Add support for additional metadata fields for Tankoubons | Add a filter by name in the GET for Tankoubons

---
+++ lib/LANraragi/Utils/Constants.pm
@@ -0,0 +1,19 @@
+package LANraragi::Utils::Constants;
+
+use strict;
+use warnings;
+use utf8;
+no warnings 'experimental';
+
+use Exporter 'import';
+our @EXPORT_OK = qw(%TANK_METADATA);
+
+
+our %TANK_METADATA = (
+        "name", 0,
+        "summary", -1,
+        "thumbhash", -2,
+        "tags", -3
+    );
+
+1;
