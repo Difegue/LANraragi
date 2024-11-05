8818db5b1cc9fc2a947ed45d30b3349f8aa9c41d
Difegue <sugoi@cock.li>
2024-11-05 18:52:45 +0100

    (#1076) Remove the default mount dir for thumbnails being a subdir of content

--- lib/LANraragi/Model/Config.pm
+++ lib/LANraragi/Model/Config.pm
@@ -78,7 +78,7 @@ sub get_redis_internal {
         server    => &get_redisad,
         debug     => $ENV{LRR_DEVSERVER} ? "1" : "0",
         reconnect => 3,
-        &get_redispassword ? (password => &get_redispassword) : ()
+        &get_redispassword ? ( password => &get_redispassword ) : ()
     );
 
     # Switch to specced database
@@ -132,7 +132,7 @@ sub get_userdir {
 sub get_thumbdir {
 
     # Content path can be overriden by LRR_THUMB_DIRECTORY
-    my $dir = &get_redis_conf( "thumbdir", get_userdir() . "/thumb" );
+    my $dir = &get_redis_conf( "thumbdir", "./thumb" );
 
     if ( $ENV{LRR_THUMB_DIRECTORY} ) {
         $dir = $ENV{LRR_THUMB_DIRECTORY};
@@ -168,8 +168,8 @@ sub get_tagrules {
     );
 }
 
-sub get_htmltitle        { return xml_escape(&get_redis_conf( "htmltitle",       "LANraragi" )) }
-sub get_motd             { return xml_escape(&get_redis_conf( "motd",            "Welcome to this Library running LANraragi!" )) }
+sub get_htmltitle        { return xml_escape( &get_redis_conf( "htmltitle", "LANraragi" ) ) }
+sub get_motd             { return xml_escape( &get_redis_conf( "motd",      "Welcome to this Library running LANraragi!" ) ) }
 sub get_tempmaxsize      { return &get_redis_conf( "tempmaxsize",     "500" ) }
 sub get_pagesize         { return &get_redis_conf( "pagesize",        "100" ) }
 sub enable_pass          { return &get_redis_conf( "enablepass",      "1" ) }
