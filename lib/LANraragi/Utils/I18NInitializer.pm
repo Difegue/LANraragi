ba3ff877307e9e7b399a67f456a3bba4eff0e0f5
Matt Gideon <117586514+Haibersut@users.noreply.github.com>
2024-12-27 13:38:18 +0800

    Update I18NInitializer.pm

--- lib/LANraragi/Utils/I18NInitializer.pm
+++ lib/LANraragi/Utils/I18NInitializer.pm
@@ -12,8 +12,9 @@ sub initialize {
     $app->helper(
         lh => sub {
             my ($c, $key, @args) = @_;
-
             my $accept_language = $c->req->headers->accept_language;
+
+            # default language
             my $lang = 'en';
 
             if ($accept_language) {
@@ -23,14 +24,15 @@ sub initialize {
             }
 
             my $handle = LANraragi::Utils::I18N->get_handle($lang);
-
             unless ($handle) {
-                $c->log->debug("No handle for language: $lang");
-                return $key;
+                $c->log->debug("No handle for language: $lang, fallback to en");
+                $handle = LANraragi::Utils::I18N->get_handle('en');
+                return $key unless $handle;
             }
 
             $c->log->debug("Key: $key, Args: " . join(", ", map { defined($_) ? $_ : 'undef' } @args));
 
+            # make sure all args are encoded in UTF-8
             my @encoded_args = map { Encode::encode('UTF-8', $_) } @args;
 
             my $translated;
@@ -42,6 +44,7 @@ sub initialize {
                 return $key;
             }
 
+            # make sure the result is decoded in UTF-8
             if ( !Encode::is_utf8($translated) ) {
                 $translated = Encode::decode_utf8($translated);
             }
@@ -53,6 +56,5 @@ sub initialize {
 
     $app->log->debug("I18N system initialized.");
 }
-
 
 1;
