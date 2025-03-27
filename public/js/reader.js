258849b2dc53614a079e9852cf24a1c82c4e5164
psilabs-dev <113860476+psilabs-dev@users.noreply.github.com>
2025-03-26 23:54:51 -0700

    disable archive hopping while in fullscreen

--- public/js/reader.js
+++ public/js/reader.js
@@ -865,6 +865,9 @@ Reader.handlePaginator = function () {
 };
 
 Reader.readPreviousArchive = function () {
+    if (window.fscreen.inFullscreen()) {
+        return;
+    }
     if (Reader.previousArchiveId !== null) {
         const newUrl = new LRR.apiURL(`/reader?id=${Reader.previousArchiveId}`).toString();
         history.replaceState(null, '', newUrl);
@@ -875,6 +878,9 @@ Reader.readPreviousArchive = function () {
 }
 
 Reader.readNextArchive = function () {
+    if (window.fscreen.inFullscreen()) {
+        return;
+    }
     if (Reader.nextArchiveId !== null) {
         const newUrl = new LRR.apiURL(`/reader?id=${Reader.nextArchiveId}`).toString();
         history.replaceState(null, '', newUrl);
