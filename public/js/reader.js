04e6e01deee5ca13fd4fda731a524466b74d4b11
psilabs-dev <113860476+psilabs-dev@users.noreply.github.com>
2025-03-28 22:43:04 -0700

    fix SO-63141448 access control checks issues

--- public/js/reader.js
+++ public/js/reader.js
@@ -838,7 +838,11 @@ Reader.initializeArchiveOverlay = function () {
         }
     };
 
-    fetch(new LRR.apiURL(`/api/archives/${Reader.id}/files/thumbnails`), { method: "POST" })
+    fetch(new LRR.apiURL(`/api/archives/${Reader.id}/files/thumbnails`), { 
+        method: "POST",
+        mode: 'same-origin',
+        credentials: 'same-origin'
+    })
         .then((response) => {
             if (response.status === 200) {
                 // Thumbnails are already generated, there's nothing to do. Very nice!
