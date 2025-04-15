4e5c0847a4bf8e0213962cc9080f2a02846a9d55
siliconfeces <124937394+siliconfeces@users.noreply.github.com>
2025-04-15 21:04:16 +0200

    Replace strings for clearing cache

--- public/js/server.js
+++ public/js/server.js
@@ -190,7 +190,7 @@ Server.triggerScript = function (namespace) {
 };
 
 Server.cleanTemporaryFolder = function () {
-    Server.callAPI("/api/tempfolder", "DELETE", I18N.CleanedTempFolder, I18N.CleanedTempFolderError, null);
+    Server.callAPI("/api/tempfolder", "DELETE", I18N.ClearCache, I18N.ClearCacheError, null);
 };
 
 Server.invalidateCache = function () {
