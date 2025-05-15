510e67d2d906f99e4f5088c5906c944f7d7b673d
Difegue <8237712+Difegue@users.noreply.github.com>
2025-05-16 00:11:51 +0200

    Enable Spanish and Norwegian

--- lib/LANraragi/Utils/I18N.pm
+++ lib/LANraragi/Utils/I18N.pm
@@ -7,10 +7,13 @@ use base 'Locale::Maketext';
 
 use Locale::Maketext::Lexicon {
     en      => [ Gettext => "../../locales/template/en.po" ],
+    es      => [ Gettext => "../../locales/template/es.po" ],
     zh      => [ Gettext => "../../locales/template/zh.po" ],
     "zh-cn" => [ Gettext => "../../locales/template/zh.po" ],
     fr      => [ Gettext => "../../locales/template/fr.po" ],
     ko      => [ Gettext => "../../locales/template/ko.po" ],
+    no      => [ Gettext => "../../locales/template/nb_NO.po" ],
+    nb      => [ Gettext => "../../locales/template/nb_NO.po" ],
     "zh-tw" => [ Gettext => "../../locales/template/zh_Hant.po" ],
     vi      => [ Gettext => "../../locales/template/vi.po" ],
     _auto   => 0,
