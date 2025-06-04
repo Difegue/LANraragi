bfdf4774419482904873b31f8554f7b945f27a73
Guerra24 <lili@guerra24.net>
2025-06-04 17:32:47 -0600

    UTF-8 wants to know your location
    Turns out perl uses ANSI win32 calls and that ensures mayhem, patch it using mt.exe so that it runs in utf-8 mode on 1903 and newer. 1809 needs to run with the global utf-8 setting enabled.

--- tools/build/msys2/install-everything.sh
+++ tools/build/msys2/install-everything.sh
@@ -3,10 +3,10 @@
 set -e
 
 # Install deps that will persist into the vfs
-pacman --needed -S mingw-w64-ucrt-x86_64-perl mingw-w64-ucrt-x86_64-openssl mingw-w64-ucrt-x86_64-imagemagick mingw-w64-ucrt-x86_64-libjxl mingw-w64-ucrt-x86_64-libheif mingw-w64-ucrt-x86_64-ghostscript mingw-w64-ucrt-x86_64-zlib mingw-w64-ucrt-x86_64-lzo2 mingw-w64-ucrt-x86_64-libarchive mingw-w64-ucrt-x86_64-ca-certificates libxcrypt --noconfirm
+pacman --needed -S mingw-w64-ucrt-x86_64-perl mingw-w64-ucrt-x86_64-openssl mingw-w64-ucrt-x86_64-imagemagick mingw-w64-ucrt-x86_64-libjxl mingw-w64-ucrt-x86_64-libheif mingw-w64-ucrt-x86_64-ghostscript mingw-w64-ucrt-x86_64-zlib mingw-w64-ucrt-x86_64-lzo2 mingw-w64-ucrt-x86_64-libarchive mingw-w64-ucrt-x86_64-ca-certificates libxcrypt unzip --noconfirm
 
 # Install temporary deps for compilation
-pacman --needed -S mingw-w64-ucrt-x86_64-gcc mingw-w64-ucrt-x86_64-make make mingw-w64-ucrt-x86_64-diffutils libbz2-devel patch mingw-w64-ucrt-x86_64-nodejs mingw-w64-ucrt-x86_64-tools-git unzip --noconfirm
+pacman --needed -S mingw-w64-ucrt-x86_64-gcc mingw-w64-ucrt-x86_64-make make mingw-w64-ucrt-x86_64-diffutils libbz2-devel patch mingw-w64-ucrt-x86_64-nodejs mingw-w64-ucrt-x86_64-tools-git --noconfirm
 
 # Install cpanm
 curl -L https://cpanmin.us | perl - App::cpanminus
@@ -45,7 +45,7 @@ cd ..
 perl ./tools/install.pl install-full
 
 # Remove compilation deps
-pacman -Rs mingw-w64-ucrt-x86_64-gcc mingw-w64-ucrt-x86_64-make make mingw-w64-ucrt-x86_64-diffutils libbz2-devel patch mingw-w64-ucrt-x86_64-nodejs mingw-w64-ucrt-x86_64-tools-git unzip --noconfirm
+pacman -Rs mingw-w64-ucrt-x86_64-gcc mingw-w64-ucrt-x86_64-make make mingw-w64-ucrt-x86_64-diffutils libbz2-devel patch mingw-w64-ucrt-x86_64-nodejs mingw-w64-ucrt-x86_64-tools-git --noconfirm
 
 # Cleanup
 rm -rf ./public/js/vendor/*.map ./public/css/vendor/*.map
