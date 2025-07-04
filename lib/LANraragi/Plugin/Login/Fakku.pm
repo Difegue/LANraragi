3ca8906f7e8851e751057ffea20ca54336c61ff1
Nixis198 <gager198@gmail.com>
2025-07-04 00:36:02 +0000

    Added a default UA, if one is not given.

--- lib/LANraragi/Plugin/Login/Fakku.pm
+++ lib/LANraragi/Plugin/Login/Fakku.pm
@@ -15,8 +15,9 @@ sub plugin_info {
         namespace   => "fakkulogin",
         author      => "Nodja, Nixis198",
         version     => "0.2",
-        description => "Handles login to fakku.",
-        parameters  => [ { type => "string", desc => "fakku_sid cookie value" }, { type => "string", desc => 'Useragent value' } ]
+        description =>
+          "Handles login to FAKKU. If the FAKKU metadata plugin stops working, update your 'fakku_sid' cookie and add your own Useragent.",
+        parameters => [ { type => "string", desc => "fakku_sid cookie value" }, { type => "string", desc => 'Useragent value' } ]
     );
 
 }
@@ -24,10 +25,21 @@ sub plugin_info {
 sub do_login {
 
     shift;
-    my ( $fakku_sid, $useragent ) = @_;
+    my ( $fakku_sid, $useragentcustom ) = @_;
+
+    my $useragent;
+    my $useragentdefault =
+      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36';
 
     my $logger = get_logger( "Fakku Login", "plugins" );
     my $ua     = Mojo::UserAgent->new;
+
+    # If the user didn't provide a useragent use the default one
+    if ( $useragentcustom eq "" ) {
+        $useragent = $useragentdefault;
+    } else {
+        $useragent = $useragentcustom;
+    }
 
     if ( $fakku_sid ne "" && $useragent ne "" ) {
         $logger->info("Cookie provided ($fakku_sid)!");
@@ -40,6 +52,7 @@ sub do_login {
             )
         );
 
+        $logger->debug("Using Useragent: ($useragent)!");
         $ua->transactor->name($useragent);
     } else {
         $logger->info("No cookies provided, returning blank UserAgent.");
