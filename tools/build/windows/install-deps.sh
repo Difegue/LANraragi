b11b55b28da05245e4f17173225eb1acf37f2cd6
siliconfeces <124937394+siliconfeces@users.noreply.github.com>
2025-09-07 00:13:21 +0200

    Remove duplicate install of libvips on windows

--- tools/build/windows/install-deps.sh
+++ tools/build/windows/install-deps.sh
@@ -5,8 +5,5 @@ set -e
 # Install deps that will persist into the vfs
 pacman --needed -S mingw-w64-ucrt-x86_64-perl mingw-w64-ucrt-x86_64-openssl mingw-w64-ucrt-x86_64-imagemagick mingw-w64-ucrt-x86_64-libjxl mingw-w64-ucrt-x86_64-libheif mingw-w64-ucrt-x86_64-ghostscript mingw-w64-ucrt-x86_64-zlib mingw-w64-ucrt-x86_64-lzo2 mingw-w64-ucrt-x86_64-libarchive mingw-w64-ucrt-x86_64-ca-certificates mingw-w64-ucrt-x86_64-libvips libxcrypt unzip --noconfirm
 
-# Install libvips, used for very speedy image operations
-pacman --needed -S mingw-w64-ucrt-x86_64-libvips
-
 # Install temporary deps for compilation
 pacman --needed -S mingw-w64-ucrt-x86_64-gcc mingw-w64-ucrt-x86_64-make make mingw-w64-ucrt-x86_64-diffutils libbz2-devel patch mingw-w64-ucrt-x86_64-nodejs mingw-w64-ucrt-x86_64-tools-git --noconfirm
