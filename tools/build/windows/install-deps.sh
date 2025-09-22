98493d13f3fb385deb3a8f039a58b5b29c4dd55d
Guerra24 <lili@guerra24.net>
2025-09-22 17:52:33 -0600

    Update packages

--- tools/build/windows/install-deps.sh
+++ tools/build/windows/install-deps.sh
@@ -3,7 +3,7 @@
 set -e
 
 # Install deps that will persist into the vfs
-pacman --needed -S mingw-w64-ucrt-x86_64-perl mingw-w64-ucrt-x86_64-openssl mingw-w64-ucrt-x86_64-imagemagick mingw-w64-ucrt-x86_64-libjxl mingw-w64-ucrt-x86_64-libheif mingw-w64-ucrt-x86_64-ghostscript mingw-w64-ucrt-x86_64-zlib mingw-w64-ucrt-x86_64-lzo2 mingw-w64-ucrt-x86_64-libarchive mingw-w64-ucrt-x86_64-ca-certificates libxcrypt unzip --noconfirm
+pacman --needed -S mingw-w64-ucrt-x86_64-perl mingw-w64-ucrt-x86_64-perl-win32-api mingw-w64-ucrt-x86_64-openssl mingw-w64-ucrt-x86_64-imagemagick mingw-w64-ucrt-x86_64-libjxl mingw-w64-ucrt-x86_64-libheif mingw-w64-ucrt-x86_64-ghostscript mingw-w64-ucrt-x86_64-zlib mingw-w64-ucrt-x86_64-lzo2 mingw-w64-ucrt-x86_64-libarchive mingw-w64-ucrt-x86_64-ca-certificates libxcrypt unzip --noconfirm
 
 # Install temporary deps for compilation
 pacman --needed -S mingw-w64-ucrt-x86_64-gcc mingw-w64-ucrt-x86_64-make make mingw-w64-ucrt-x86_64-diffutils libbz2-devel patch mingw-w64-ucrt-x86_64-nodejs mingw-w64-ucrt-x86_64-tools-git --noconfirm
