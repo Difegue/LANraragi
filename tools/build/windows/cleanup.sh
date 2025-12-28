04c568681099cd8546b33f9a96576b3622fcbe69
Lili González <lili@guerra24.net>
2025-12-27 21:18:44 -0600

    Fix renamed msys2 dependency

--- tools/build/windows/cleanup.sh
+++ tools/build/windows/cleanup.sh
@@ -3,7 +3,7 @@
 set -e
 
 # Remove compilation deps
-pacman -Rs mingw-w64-ucrt-x86_64-gcc mingw-w64-ucrt-x86_64-make make mingw-w64-ucrt-x86_64-diffutils libbz2-devel patch mingw-w64-ucrt-x86_64-nodejs mingw-w64-ucrt-x86_64-tools-git --noconfirm
+pacman -Rs mingw-w64-ucrt-x86_64-gcc mingw-w64-ucrt-x86_64-make make mingw-w64-ucrt-x86_64-diffutils libbz2-devel patch mingw-w64-ucrt-x86_64-nodejs mingw-w64-ucrt-x86_64-tools --noconfirm
 
 # Cleanup
 rm -rf ./public/js/vendor/*.map ./public/css/vendor/*.map
