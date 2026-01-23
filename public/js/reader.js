1f22e4db0418b72aa0b30f05ceff25284cf1ae6a
Lili <lili@guerra24.net>
2026-01-23 14:10:48 -0600

    Fix chapter select going to the wrong page and not clearing after removing all entries

--- public/js/reader.js
+++ public/js/reader.js
@@ -1171,8 +1171,10 @@ Reader.updateArchiveOverlay = function (forceUpdate = false) {
         $(".chapter-selector").html(chapterOptions);
 
         $("#chapter-select").off("change").on("change", function () {
-            Reader.goToPage($(this).val());
+            Reader.goToPage($(this).val() - 1);
         });
+    } else {
+        $(".chapter-selector").html("");
     }
 
     // For each link in the pages array, craft a div and jam it in the overlay.
