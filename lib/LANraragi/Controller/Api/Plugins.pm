494a3c657905fe3edbe2190a3e19325c48491cd9
psilabs-dev <113860476+psilabs-dev@users.noreply.github.com>
2026-04-12 20:15:44 -0700

    move update_plugin_config over to Api/Plugins.pm

---
+++ lib/LANraragi/Controller/Api/Plugins.pm
@@ -0,0 +1,52 @@
+package LANraragi::Controller::Api::Plugins;
+use Mojo::Base 'Mojolicious::Controller';
+
+use LANraragi::Utils::Generic qw(render_api_response exec_with_lock);
+
+sub update_plugin_config {
+    my $self      = shift->openapi->valid_input or return;
+    my $namespace = $self->stash('plugin_namespace');
+    my $body      = $self->req->json;
+
+    return unless exec_with_lock(
+        $self,
+        "plugin-write:$namespace",
+        "update_plugin_config",
+        $namespace,
+        sub {
+            my $redis   = $self->LRR_CONF->get_redis_config;
+            my $namerds = "LRR_PLUGIN_" . uc($namespace);
+
+            unless ( $redis->exists($namerds) ) {
+                $redis->quit();
+                $self->render(
+                    openapi => {
+                        operation => "update_plugin_config",
+                        error     => "Plugin '$namespace' doesn't exist on the server.",
+                        success   => 0,
+                    },
+                    status => 404
+                );
+                return;
+            }
+
+            if ( exists $body->{enabled} ) {
+                $redis->hset( $namerds, "enabled", $body->{enabled} ? "1" : "0" );
+            }
+
+            if ( exists $body->{hidden} ) {
+                $redis->hset( $namerds, "hidden", $body->{hidden} ? "1" : "0" );
+            }
+
+            if ( exists $body->{priority} ) {
+                $redis->hset( $namerds, "priority", $body->{priority} );
+            }
+
+            $redis->quit();
+
+            render_api_response( $self, "update_plugin_config" );
+        }
+    );
+}
+
+1;
