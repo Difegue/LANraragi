8733558a910f8465125ebdbef60d07724c7c6a8a
siliconfeces <124937394+siliconfeces@users.noreply.github.com>
2026-07-05 02:46:31 +0200

    Make eslint ignore everything in public/js/vendor, including subdirectories

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -79,6 +79,6 @@ const config = {
 
 export default defineConfig([
     importX.flatConfigs.recommended,
-    globalIgnores(["public/js/vendor/*.js", "tests/samples/*"]),
+    globalIgnores(["public/js/vendor/**", "tests/samples/*"]),
     config,
 ]);
